5ea1a8a402d0d09c17c50dc9616ab4e0d22b654c
gavin <380274+gavxin@users.noreply.github.com>
2023-06-21 18:10:57 +0800

    modify switch header/source key mapping

--- mappings.lua
+++ mappings.lua
@@ -1,6 +1,6 @@
 return {
   n = {
     ["<leader>v"] = { "<C-v>", desc = "Vertical Select" },
-    ["<leader>ls"] = { "<cmd> ClangdSwitchSourceHeader <cr>", desc = "Switch between C/C++ header and source files" }, 
+    ["<leader>lo"] = { "<cmd> ClangdSwitchSourceHeader <cr>", desc = "Switch between C/C++ header and source files" }, 
   }
 }
