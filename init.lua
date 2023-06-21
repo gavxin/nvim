6c54d220976c33ff8c288c188b7fe6a2090f3865
gavin <380274+gavxin@users.noreply.github.com>
2023-06-21 16:22:52 +0800

    config lsp for c/c++

--- init.lua
+++ init.lua
@@ -1,1 +1,16 @@
-return {}
+return {
+  -- for c/c++
+  lsp = {
+    setup_handlers = {
+      -- add custom handler
+      clangd = function(_, opts) require("clangd_extensions").setup { server = opts } end,
+    },
+    config = {
+      clangd = {
+        capabilities = {
+          offsetEncoding = "utf-8",
+        },
+      },
+    },
+  },
+}
---
+++ plugins/clangd_extensions.lua
@@ -0,0 +1,9 @@
+return {
+  "p00f/clangd_extensions.nvim", -- install lsp plugin
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = { "clangd" }, -- automatically install lsp
+    },
+  },
+}
