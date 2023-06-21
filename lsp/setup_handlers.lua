b9077daa783cadb13035e35fc8bf54150ad77aa4
gavin <380274+gavxin@users.noreply.github.com>
2023-06-22 01:25:02 +0800

    use community rust pack

--- lsp/setup_handlers.lua
+++ lsp/setup_handlers.lua
@@ -2,7 +2,4 @@ return {
   clangd = function(_, opts)
     require("clangd_extensions").setup { server = opts }
   end,
-  rust_analyzer = function(_, opts)
-    require("rust-tools").setup { server = opts }
-  end,
 }
--- plugins/rust-tools.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "simrat39/rust-tools.nvim",
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {
-      ensure_installed = { "rust_analyzer" },
-    },
-  },
-}
