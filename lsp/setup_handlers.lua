17e25ac9886e1fef1ce972d6eb7ba806b48738f6
gavin <380274+gavxin@users.noreply.github.com>
2023-06-21 17:27:11 +0800

    fix

--- lsp/setup_handlers.lua
+++ lsp/setup_handlers.lua
@@ -1,9 +1,7 @@
 return {
-  setup_handlers = {
-    clangd = function(_, opts)
-      require("clangd_extensions").setup {
-        server = opts
-      }
-    end,
-  },
+  clangd = function(_, opts)
+    require("clangd_extensions").setup {
+      server = opts
+    }
+  end,
 }
